9478444e0fdabfb4888031621c515e5020ae88b2
Arvinder-Singh-Ghumman <arvindersinghghumman@gmail.com>
2024-03-12 21:59:35 -0600

    login and signup

--- script.js
+++ script.js
@@ -1,40 +1,32 @@
 var userName,userPhone,userEmail,userRole;
-var users = URL("./users.js");
+var users = "./users.js";
+console.log(users);
+var isLoggedin = JSON.parse(sessionStorage.getItem("user"))==null?true:false;;
 
-async function getUsers() {
-  try {
-    const response = await fetch('users.json');
-    const data = await response.json();
-
-    //storing the data in users 
-    users = data;
-
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-}
 
 //function to sign up
 function signUp(){
   //get name, phone, email, coworker and store in varirables
   var new_userName,new_userPhone,new_userEmail,new_userRole,new_userPassword;
 
-  getUsers().then((val)=>{
     //change the colour of input box, display a message alerting that username already exists
-    if(users.find(obj=>obj.name==new_userName))
-      alert("username already exists");
+    if(new_userName.length()<3)
+      alert("Name too small");
     else if(users.find(obj=>obj.email==new_userEmail))
       alert("email already registered");
     else{
       users.push({"name":new_userName,"password":new_userPassword,"phone":new_userPhone,"email":new_userEmail,"role":new_userRole})
-      fs.writeFileSync("./users.json", JSON.stringify(users), 'utf8');
+      console.log(users);
+    }  
+}
 
-    }
-
-  })
-
-  
-
-
-
+function logIn(){
+  console(isLoggedin);
+  var given_username="John Doe",given_password="abc";
+  var index = users.find(abc=>abc.name==given_username);
+  if(index.password==given_password){
+    sessionStorage.setItem("user",JSON.stringify(index));
+  }else{
+    alert("wrong password");
+  }
 }
