60da7479ec96b5c75236355d957caeced13cd4e3
Arvinder-Singh-Ghumman <arvindersinghghumman@gmail.com>
2024-03-12 20:37:07 -0600

    script.js and html

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-import users from "./users.json";
-
 var userName,userPhone,userEmail,userRole;
-const users = JSON.parse(users);
+var users = URL("./users.js");
 
+async function getUsers() {
+  try {
+    const response = await fetch('users.json');
+    const data = await response.json();
+
+    //storing the data in users 
+    users = data;
+
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
 
 //function to sign up
 function signUp(){
-  var new_userName,new_userPhone,new_userEmail,new_userRole;
-
   //get name, phone, email, coworker and store in varirables
-  console.log(users.find(obj=>obj.name=="Jane Smith"));
+  var new_userName,new_userPhone,new_userEmail,new_userRole,new_userPassword;
+
+  getUsers().then((val)=>{
+    //change the colour of input box, display a message alerting that username already exists
+    if(users.find(obj=>obj.name==new_userName))
+      alert("username already exists");
+    else if(users.find(obj=>obj.email==new_userEmail))
+      alert("email already registered");
+    else{
+      users.push({"name":new_userName,"password":new_userPassword,"phone":new_userPhone,"email":new_userEmail,"role":new_userRole})
+      fs.writeFileSync("./users.json", JSON.stringify(users), 'utf8');
+
+    }
+
+  })
+
+  
 
 
 
--- users.js
+++ users.js
@@ -1,30 +1,35 @@
-[
+var users = [
   {
     "name": "John Doe",
+    "password":"abc",
     "phone": "+1234567890",
     "email": "john.doe@example.com",
     "role": "user"
   },
   {
     "name": "Jane Smith",
+    "password":"abcd",
     "phone": "+9876543210",
     "email": "jane.smith@example.com",
     "role": "admin"
   },
   {
     "name": "Alice Johnson",
+    "password":"abcde",
     "phone": "+1122334455",
     "email": "alice.johnson@example.com",
     "role": "user"
   },
   {
     "name": "Bob Anderson",
+    "password":"abcdef",
     "phone": "+5544332211",
     "email": "bob.anderson@example.com",
     "role": "user"
   },
   {
     "name": "Eva Williams",
+    "password":"abcdefg",
     "phone": "+9988776655",
     "email": "eva.williams@example.com",
     "role": "admin"
