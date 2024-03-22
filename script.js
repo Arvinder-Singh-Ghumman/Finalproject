ca1b47ce37a952f668a87c85598203e0b8f470f3
ashu-369 <145891277+ashu-369@users.noreply.github.com>
2024-03-22 13:40:34 -0600

    recoevring

--- script.js
+++ script.js
@@ -1,56 +1,3 @@
-<<<<<<< HEAD
-var userName, userPhone, userEmail, userRole;
-// var users = URL("./users.js");
-var users = [
-  { name: "John", email: "john@example.com", password: "password1" },
-  { name: "Jane", email: "jane@example.com", password: "password2" }
-];
-console.log(users);
-var isLoggedIn = JSON.parse(sessionStorage.getItem("user")) == null ? true : false;
-
-
-//function to sign up
-function signUp() {
-  //get name, phone, email, coworker and store in variables
-  var new_userName, new_userPhone, new_userEmail, new_userRole, new_userPassword;
-  new_userName = document.querySelector('#_name_').value;
-  new_userPhone = document.querySelector('#_phone_').value;
-  new_userEmail = document.querySelector('#_email_').value;
-  new_userPassword = document.querySelector('#_password_').value;
-  new_userRole = document.querySelector('#_role_').value;
-
-  //change the color of input box, display a message alerting that username already exists
-  if (new_userName.length() < 3)
-    alert("Name too small");
-  else if (users.find(obj => obj.email == new_userEmail))
-    alert("email already registered");
-  else {
-    users.push({
-      "name": new_userName,
-      "password": new_userPassword,
-      "phone": new_userPhone,
-      "email": new_userEmail,
-      "role": new_userRole
-    })
-    console.log(users);
-  }
-}
-
-function logIn() {
-  console.log("isLoggedIn");
-  // var given_username="John Doe",given_password="abc";
-  var given_email = document.querySelector("#_email").value;
-  var given_password = document.querySelector("#_password").value;
-
-  var index = users.find(abc => abc.email === given_email);
-  if (index) {
-    if (index.password === given_password) {
-      openPage('ownerhome.html');
-      sessionStorage.setItem("user", JSON.stringify(index));
-    } else {
-      alert("wrong password");
-    }
-=======
 import { userDatabase } from "./users.js";
 var users;
 //get users if in localstorage otherwise the default user.js
@@ -110,7 +57,6 @@ function logIn(event) {
   } else if (index.password == given_password) {
     sessionStorage.setItem("user", JSON.stringify(index));
     alert("hogya");
->>>>>>> 2e45313496f0c58b2e7c832496e06524ed75d94e
   } else {
     alert("user not found");
   }
@@ -134,11 +80,6 @@ function displaySignUp() {
 
 }
 
-<<<<<<< HEAD
-function openPage(pageURL) {
-  window.location.href = pageURL;
-}
-=======
 window.onload = () => {
   document
     .querySelector("#signup")
@@ -151,4 +92,3 @@ window.onload = () => {
   document.getElementById("displaySignup").addEventListener("click", displaySignUp);
   document.getElementById("Displaylogin").addEventListener("click", displayLogIn);
 };
->>>>>>> 2e45313496f0c58b2e7c832496e06524ed75d94e
