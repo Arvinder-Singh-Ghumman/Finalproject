988473aab62903c9a6d709d8e7a21a55bc05af87
ashu-369 <145891277+ashu-369@users.noreply.github.com>
2024-03-23 13:39:29 -0600

    'l'

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ window.onload = function() {
 
 (setTimeout(function () {
   document.getElementById('loginPopUp').style.display = 'block';
-}, 10000))(); // 10000 milliseconds = 10 seconds
+}, 10))(); // 10000 milliseconds = 10 seconds
 
 function closeLoginPopUp() {
   document.getElementById('loginPopUp').style.display = 'none';
